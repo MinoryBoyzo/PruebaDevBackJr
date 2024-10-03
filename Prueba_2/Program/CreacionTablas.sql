f698991cccc0f8b3cc338a9186857297670b93b3
Minory Boyzo <91289572+MinoryBoyzo@users.noreply.github.com>
2024-10-03 08:25:47 -0600

    Add files via upload

---
+++ Prueba_2/Program/CreacionTablas.sql
@@ -0,0 +1,16 @@
+CREATE DATABASE empresa;
+USE empresa;
+CREATE TABLE usuarios (
+    userId INT AUTO_INCREMENT PRIMARY KEY,
+    Login VARCHAR(100),
+    Nombre VARCHAR(100),
+    Paterno VARCHAR(100),
+    Materno VARCHAR(100)
+);
+
+CREATE TABLE empleados (
+    userId INT,
+    Sueldo DOUBLE,
+    FechaIngreso DATE,
+    FOREIGN KEY (userId) REFERENCES usuarios(userId)
+);
